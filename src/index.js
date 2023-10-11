ffbdc53a938602e577a6f10a3215b488a02dd584
Md Musta E Nur Rahman <mnrabir@gmail.com>
2023-10-11 20:03:48 +0900

    The comment app is build upto show the comment section. Created separate component of CommentDetail and passed down props from the parent component. Used faker js to generate fake data.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+import {faker} from '@faker-js/faker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <CommentDetail userName="Bob" timeAgo="Today at 5:00pm" commentDetail="Nice blog 1" image={faker.image.avatar()} />
+            <CommentDetail userName="Sam" timeAgo="Yesterday at 12:00pm" commentDetail="Nice blog 2" image={faker.image.avatar()} />
+            <CommentDetail userName="Dud" timeAgo="12 December at 5:00pm" commentDetail="Worst blog 1" image={faker.image.avatar()} />
+        </div>
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.querySelector("#root") );
