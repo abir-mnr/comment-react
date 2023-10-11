5de3936690c63d76b28ddb9d12eee47d159ed7ef
Md Musta E Nur Rahman <mnrabir@gmail.com>
2023-10-11 20:58:27 +0900

    Comment app done. Created another component named ApprovalCard and made use of nested child component.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import {faker} from '@faker-js/faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail userName="Bob" timeAgo="Today at 5:00pm" commentDetail="Nice blog 1" image={faker.image.avatar()} />
-            <CommentDetail userName="Sam" timeAgo="Yesterday at 12:00pm" commentDetail="Nice blog 2" image={faker.image.avatar()} />
-            <CommentDetail userName="Dud" timeAgo="12 December at 5:00pm" commentDetail="Worst blog 1" image={faker.image.avatar()} />
+            <ApprovalCard>
+                <h4>Warning !</h4>
+                <div>
+                    Are you sure you want to accept this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail userName="Sam" timeAgo="Yesterday at 12:00pm" commentDetail="Nice blog 2" image={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail userName="Bob" timeAgo="Today at 5:00pm" commentDetail="Nice blog 1" image={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail userName="Dud" timeAgo="12 December at 5:00pm" commentDetail="Worst blog 1" image={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     );
 }
